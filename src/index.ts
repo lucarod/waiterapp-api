c0e915537d003681d2de09012bdb2cf75654cef5
Luca Rodrigues <luca.rodrigues@wundermanthompson.com>
2023-11-24 15:47:49 -0300

    feat: add useCases, routes, libs and models

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+import path from 'node:path';
 import express from 'express';
+import mongoose from 'mongoose';
+import { router } from './router';
 
-const app = express();
+mongoose.connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+    const port = 3001;
 
-const port = 3001;
-app.listen(port, () => {
-  console.log(`🔥 Server is running on http://localhost:${port}`);
-});
+    app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+    app.use(express.json());
+    app.use(router);
+
+    app.listen(port, () => {
+      console.log(`🔥 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar ao mongodb'));
