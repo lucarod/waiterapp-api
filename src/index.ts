c43f754e432f4079c8da70012204bb4fa725cfee
Luca Rodrigues <luca.rodrigues@wundermanthompson.com>
2023-11-30 18:04:20 -0300

    fix: allow cors

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,12 @@ mongoose.connect('mongodb://localhost:27017')
     const app = express();
     const port = 3001;
 
+    app.use((req, res) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', '*');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+    });
+
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
